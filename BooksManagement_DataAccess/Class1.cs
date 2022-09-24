d5d087e9521cbd396b1a8fdac0a69cd9c55c5edb
husseinNew <www.hussein2001n@gmail.com>
2022-09-24 10:29:54 +0300

    Implemente to BookM_DataAccess and creating localdb database

--- BooksManagement_DataAccess/Class1.cs
+++ BooksManagement_DataAccess/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace BooksManagement_DataAccess
-{
-    public class Class1
-    {
+﻿using BooksManagement_Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
 
+namespace BooksManagement_DataAccess
+{
+    public class BooksMDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source = (localdb)\\MSSQLLocalDB;" +
+                                        "Initial Catalog = BooksManagement");
+        }
     }
 }
---
+++ BooksManagement_DataAccess/Migrations/20220924072449_initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksManagement_DataAccess.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    AvailableCopies = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
